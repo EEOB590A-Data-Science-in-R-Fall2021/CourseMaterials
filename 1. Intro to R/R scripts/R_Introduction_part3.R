501ddee9629f62d36bb4d1104aaa40ca26c0883f
Rogers <haldre@ee-hrog-mba20.eeob.iastate.edu>
2021-09-13 10:18:00 -0500

    update files for today's class

--- 1. Intro to R/R scripts/R_Introduction_part3.R
+++ 1. Intro to R/R scripts/R_Introduction_part3.R
@@ -2,57 +2,56 @@
 # Intro to R - Part 3
 
 ######## Topics #######
-#1. Practice reading in Excel files, with some added complexity
-#2. Learning about dates
+# 1. Practice reading in Excel files, with some added complexity
+# 2. Learning about dates
 
-# This script requires 2 data files: 
-#"leaf damage ag expt.xls"
-#"Exploring_dates.xlsx"
+# This script requires 2 data files:
+# "leaf damage ag expt.xls"
+# "Exploring_dates.xlsx"
 
-#Start by loading the libraries you might need (hint, need one for reading in excel files)
+# Start by loading the libraries you might need (hint, need one for reading in excel files)
 
-#1) Reading in Excel files
+# 1) Reading in Excel files
 
-#We are going to work with the "leaf damage ag expt.xls" file. This is the 
-#exact file I found from an undergraduate project in 2007. It's not perfect. 
-#Let's see how we can fix it. 
+# We are going to work with the "leaf damage ag expt.xls" file. This is the
+# exact file I found from an undergraduate project in 2007. It's not perfect.
+# Let's see how we can fix it.
 
-#1.a - read in the "beans" worksheet and give it the same name as the worksheet tab it came from
+# 1.a - read in the "beans" worksheet and give it the same name as the worksheet tab it came from
 
-# Look at the structure of 'beans'. What class are each of the columns in? 
+# Look at the structure of 'beans'. What class are each of the columns in?
 
-#Read in the beans worksheet again, and tell R the appropriate class/column type 
-#for each column. Note that read_excel doesn't let you choose factor, so use text instead. Give this dataframe a new name so you can compare to earlier "beans" df. 
+# Read in the beans worksheet again, and tell R the appropriate class/column type
+# for each column. Note that read_excel doesn't let you choose factor, so use text instead. Give this dataframe a new name so you can compare to earlier "beans" df.
 
-#Check to make sure the columns are now in the appropriate class/column type
-
-
-#After you read it in, you realize that the "Number" column indicates the ID 
-#of each exclosure, and therefore should be a factor. Change that column to a
-#factor and then check to make sure it is actually a factor
-
-#check the number of levels for this factor to make sure it converted correctly
-
-#1.b - Herbivory Worksheet
-#read in the "herbivory" worksheet and give it the same name as the worksheet it came from
-
-#Notice that there are some X's in the leaflet columns of the herbivory worksheet?
-#Read it in again, but this time tell R that the X means NA, and give the dataframe a new  name so you can compare. 
-
-#Read the herbivory datasheet in again, but pull in everything but columns L and M because they do not belong with rows 2-6. If you aren't sure how to do this, look at the help file for the function that reads in excel files
+# Check to make sure the columns are now in the appropriate class/column type
 
 
-#2) Dealing with dates
+# After you read it in, you realize that the "Number" column indicates the ID
+# of each exclosure, and therefore should be a factor. Change that column to a
+# factor and then check to make sure it is actually a factor
 
-#Read in the "Exploring_dates.xlsx" file. 
+# check the number of levels for this factor to make sure it converted correctly
 
-#Is this object a vector, matrix, dataframe, tibble, array or list? 
+# 1.b - Herbivory Worksheet
+# read in the "herbivory" worksheet and give it the same name as the worksheet it came from
 
-#The name of this object is kinda unwieldy. Rename the object "dates"
+# Notice that there are some X's in the leaflet columns of the herbivory worksheet?
+# Read it in again, but this time tell R that the X means NA, and give the dataframe a new  name so you can compare.
 
-#What format does R recognize each of the dates as? Which ones did not read in 
-#as dates? 
+# Read the herbivory datasheet in again, but pull in everything but columns L and M because they do not belong with rows 2-6. If you aren't sure how to do this, look at the help file for the function that reads in excel files
 
-#Create a new column based on the difference in time between date 1 and date 2 
-#and name it "duration" and add it to the "dates" dataframe
 
+# 2) Dealing with dates
+
+# Read in the "Exploring_dates.xlsx" file.
+
+# Is this object a vector, matrix, dataframe, tibble, array or list?
+
+# The name of this object is kinda unwieldy. Rename the object "dates"
+
+# What format does R recognize each of the dates as? Which ones did not read in
+# as dates?
+
+# Create a new column based on the difference in time between date 1 and date 2
+# and name it "duration" and add it to the "dates" dataframe
