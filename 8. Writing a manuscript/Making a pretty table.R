47e6c5b9ab51eda57bb61e94f8980091a20b89cc
Rogers <haldre@ee-hrog-mba20.eeob.iastate.edu>
2021-12-06 10:15:04 -0600

    update older files

--- 8. Writing a manuscript/Making a pretty table.R
+++ 8. Writing a manuscript/Making a pretty table.R
@@ -11,15 +11,15 @@ library(gt) # for making pretty tables
 library(webshot) # for saving gt objects as pdf or png
 
 # 2. Load data -------
-transplant<-read.csv("data/tidy/transplant_tidy_clean.csv", header=T)
+transplant <- read.csv("data/tidy/transplant_tidy_clean.csv", header=T)
 
 # 3. Run model -------
 webmod1 <- lm(websize ~ island * native, transplant)
 summary(webmod1)
 
 # 4. Make results from model into tidy table/tibble -------
-tidywebmod <- tidy(webmod1) # the broom function
-tbl_regression(webmod1) #the gt summary fundtion 
+tidywebmod <- tidy(webmod1) # the broom package
+tbl_regression(webmod1) #the gtsummary package 
 
 #other tables you can create from model object using broom function
 augment(webmod1) 
@@ -34,7 +34,7 @@ webmodtable <- tidywebmod %>%
 # 6. Save table ----------
 # This is actually a bit trickier than I'd like because gt produces an html file, and then you have to rely on another package (webshot) and an executable (PhantomJS) to get a screenshot of the html. 
 
-install_phantomjs()
+install_phantomjs() # only need to install once
 
 webmodtable %>%
   gtsave("8. Writing a manuscript/tables/webmodtable.png")
